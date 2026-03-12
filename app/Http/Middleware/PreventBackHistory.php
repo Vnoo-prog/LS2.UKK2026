cd15b4ce88f578cb16512024f811b3c4e1eccc9b
Vnoo-prog <ahmadsulfino@gmail.com>
2026-03-12 15:03:28 +0700

    Update PreventBackHistory.php
    Removed comment about preventing browser cache.

--- app/Http/Middleware/PreventBackHistory.php
+++ app/Http/Middleware/PreventBackHistory.php
@@ -11,8 +11,6 @@ class PreventBackHistory
     public function handle(Request $request, Closure $next): Response
     {
         $response = $next($request);
-        
-        // Mencegah browser menyimpan cache halaman
         return $response->header('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
                         ->header('Pragma', 'no-cache')
                         ->header('Expires', 'Sat, 26 Jul 1997 05:00:00 GMT');
